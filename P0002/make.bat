f521447bf2392e1f6b97255b1a5d89f1a029530b
Dan Stevens <togos00@gmail.com>
2023-03-09 19:37:40 -0600

    Remove rules to make Interpreter.dll

--- P0002/make.bat
+++ P0002/make.bat
@@ -3,6 +3,5 @@ set "csc_opts="
 :set "csc_opts=/debug:full"
 
 call csc /out:TS34Interpreter.exe %csc_opts% /r:System.Net.Http.dll %cs_source_files%
-call csc /out:TS34Interpreter.dll %csc_opts% /r:System.Net.Http.dll /target:library %cs_source_files%
 
 call csc /out:LibTest.exe /main:TOGoS.TScrpt34_2.LibTest %csc_opts% /r:System.Net.Http.dll /resource:TS34Interpreter.exe /target:exe LibTest.cs
