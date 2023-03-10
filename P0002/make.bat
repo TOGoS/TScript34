d2c431c8824d5d832815d3149a5e450ae85e748e
Dan Stevens <togos00@gmail.com>
2023-03-09 19:41:44 -0600

    Simplify make.bat/Makefile further

--- P0002/make.bat
+++ P0002/make.bat
@@ -4,4 +4,4 @@ set "csc_opts="
 
 call csc /out:TS34Interpreter.exe %csc_opts% /r:System.Net.Http.dll %cs_source_files%
 
-call csc /out:LibTest.exe /main:TOGoS.TScrpt34_2.LibTest %csc_opts% /r:System.Net.Http.dll /resource:TS34Interpreter.exe /target:exe LibTest.cs
+call csc /out:LibTest.exe /main:TOGoS.TScrpt34_2.LibTest %csc_opts% /r:System.Net.Http.dll /target:exe LibTest.cs %cs_source_files%
