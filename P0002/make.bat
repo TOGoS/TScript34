b25f53bd0acee6db93edb07465865cf19060fc7b
TOGoS <togos00@gmail.com>
2023-03-12 00:13:02 -0600

    make.bat: quit on error

--- P0002/make.bat
+++ P0002/make.bat
@@ -3,5 +3,7 @@ set "csc_opts="
 :set "csc_opts=/debug:full"
 
 call csc /out:TS34Interpreter.exe %csc_opts% /r:System.Net.Http.dll %cs_source_files%
+if errorlevel 1 exit /B 1
 
 call csc /out:LibTest.exe /main:TOGoS.TScrpt34_2.LibTest %csc_opts% /r:System.Net.Http.dll /target:exe LibTest.cs %cs_source_files%
+if errorlevel 1 exit /B 1
