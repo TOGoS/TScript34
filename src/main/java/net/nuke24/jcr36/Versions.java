2d409561a596ee60560f893ebf0e97cf0cc42825
TOGoS <togos00@gmail.com>
2023-07-11 22:09:03 +0000

    Bump version to 36.0.9-dev

--- src/main/java/net/nuke24/jcr36/Versions.java
+++ src/main/java/net/nuke24/jcr36/Versions.java
@@ -3,6 +3,6 @@ package net.nuke24.jcr36;
 public class Versions {
 	private Versions() { }
 	
-	public static final String JCR_VERSION = "36.0.8";
+	public static final String JCR_VERSION = "36.0.9-dev";
 	public static final String JCR_NAME_AND_VERSION = "JavaCommandRunner"+JCR_VERSION;
 }
