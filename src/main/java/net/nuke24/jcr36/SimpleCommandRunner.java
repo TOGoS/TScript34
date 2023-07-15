bdc7c70362b6baf0b4fdc85c41905a3c7a6a14b0
TOGoS <togos00@gmail.com>
2023-07-14 19:18:21 -0500

    Clean up imports in SimpleCommandRunner

--- src/main/java/net/nuke24/jcr36/SimpleCommandRunner.java
+++ src/main/java/net/nuke24/jcr36/SimpleCommandRunner.java
@@ -1,19 +1,10 @@
 package net.nuke24.jcr36;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import net.nuke24.jcr36.ActionRunner.QuitException;
 import net.nuke24.jcr36.action.JCRAction;
-import net.nuke24.jcr36.action.Null;
-import net.nuke24.jcr36.action.Print;
-import net.nuke24.jcr36.action.Quit;
-import net.nuke24.jcr36.action.SerialAction;
 
 public class SimpleCommandRunner {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		JCRAction action = SimpleCommandParser.parseDoCmd(args);
 		try {
 			ActionRunner actionRunner = new ActionRunner();
