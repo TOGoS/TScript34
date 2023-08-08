3f9aabd7b8c7ddb735b26b25d6969ffcf54f2802
TOGoS <togos00@gmail.com>
2023-08-08 13:04:11 -0500

    JCR36.1.10: --help and --version, rm non-simpler
    i.e. what 588776455ea0de2e4d40566a3f3188bb01801358
    said it did, but somehow failed to.

--- P0008/src/main/java/net/nuke24/jcr36/Function.java
+++
@@ -1,7 +0,0 @@
-package net.nuke24.jcr36;
-
-// For JRE-1.6 compatibility
-// Equivalent to java.util.function.Function
-public interface Function<I,O> {
-	public O apply(I in);
-}
--- P0008/src/main/java/net/nuke24/jcr36/Streams.java
+++
@@ -1,9 +0,0 @@
-package net.nuke24.jcr36;
-
-public class Streams {
-	private Streams() { }
-	
-	public static final int STDIN_FD = 0;
-	public static final int STDOUT_FD = 1;
-	public static final int STDERR_FD = 2;
-}
