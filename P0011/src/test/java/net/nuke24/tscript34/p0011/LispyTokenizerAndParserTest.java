fcee47b8a53e38950e9edcf03572f95c7d42ea60
TOGoS <togos00@gmail.com>
2024-02-01 15:47:16 -0600

    Rename LispyIntegrationTest to LispyTokenizerAndParserTest

--- P0011/src/test/java/net/nuke24/tscript34/p0011/LispyTokenizerAndParserTest.java
+++ P0011/src/test/java/net/nuke24/tscript34/p0011/LispyTokenizerAndParserTest.java
@@ -6,7 +6,7 @@ import net.nuke24.tscript34.p0011.sexp.ConsPair;
 import net.nuke24.tscript34.p0011.sexp.LiteralValue;
 import net.nuke24.tscript34.p0011.sexp.Symbols;
 
-public class LispyIntegrationTest extends XXTestCase {
+public class LispyTokenizerAndParserTest extends XXTestCase {
 	public void testParseNilSource() {
 		String source = "()";
 		Tokenizer t = new Tokenizer(LispyCharDecoder.instance, "test.sexp");
