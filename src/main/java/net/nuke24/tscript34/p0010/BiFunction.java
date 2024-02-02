138b6db6d78c771a49cb3e762697b626c97be7fb
TOGoS <togos00@gmail.com>
2024-02-02 11:09:18 -0600

    TScript34-P0010-v0.0.2: Add Function, BiFunction interfaces

---
+++ src/main/java/net/nuke24/tscript34/p0010/BiFunction.java
@@ -0,0 +1,6 @@
+package net.nuke24.tscript34.p0010;
+
+/** Equivalent to java.util.function.BiFunction */
+public interface BiFunction<T, U, R> {
+	R apply(T t, U u);
+}
---
+++ src/main/java/net/nuke24/tscript34/p0010/Function.java
@@ -0,0 +1,6 @@
+package net.nuke24.tscript34.p0010;
+
+/** Equivalent to java.util.function.Function */
+public interface Function<T, R> {
+	public R apply(T t);
+}
