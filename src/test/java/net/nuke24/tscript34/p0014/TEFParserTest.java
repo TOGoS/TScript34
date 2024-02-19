8f36642e30ac1c076105c0321fa59264da93ea94
TOGoS <togos00@gmail.com>
2024-02-19 17:01:44 -0600

    Make chunk merging a Danducer

--- src/test/java/net/nuke24/tscript34/p0014/TEFParserTest.java
+++ src/test/java/net/nuke24/tscript34/p0014/TEFParserTest.java
@@ -7,8 +7,10 @@ import java.util.List;
 import java.util.Random;
 
 import junit.framework.TestCase;
+import net.nuke24.tscript34.p0010.Function;
 import net.nuke24.tscript34.p0010.ducer.DucerChunk;
 import net.nuke24.tscript34.p0010.ducer.DucerState;
+import net.nuke24.tscript34.p0010.ducer.InputPortState;
 import net.nuke24.tscript34.p0014.LLChunks.Chunk;
 import net.nuke24.tscript34.p0014.LLChunks.ContentPiece;
 import net.nuke24.tscript34.p0014.LLChunks.Header;
@@ -21,47 +23,77 @@ public class TEFParserTest extends TestCase
 {
 	static final Charset UTF8 = Charset.forName("UTF-8");
 	
-	// Merge the chunks, or return null if not mergeable
-	protected static Chunk merge( Chunk a, Chunk b ) {
-		if( a instanceof HeaderKey hka && b instanceof HeaderValuePiece vcb ) {
-			return new Header(hka.key(), vcb.data());
+	static class LLChunkMerger implements Function<DucerChunk<Chunk[]>, DucerState<Chunk[],Chunk[]>> {
+		protected static final Chunk[] EMPTY_CHUNK_LIST = new Chunk[0];
+		protected static final InputPortState<Chunk[]> EMPTY_OPEN_INPUT = new InputPortState<Chunk[]>(false, EMPTY_CHUNK_LIST);
+		protected static final InputPortState<Chunk[]> EMPTY_CLOSED_INPUT = new InputPortState<Chunk[]>(true, EMPTY_CHUNK_LIST);
+		public static final LLChunkMerger INIT = new LLChunkMerger(null);
+		
+		final Chunk previousChunk;
+		
+		private LLChunkMerger(Chunk previousChunk) {
+			this.previousChunk = previousChunk;
 		}
-		if( a instanceof Header ha && b instanceof HeaderValuePiece vcb ) {
-			return new Header(ha.key(), ha.value()+vcb.data());
+		
+		// Merge the chunks, or return null if not mergeable
+		protected static Chunk merge( Chunk a, Chunk b ) {
+			if( a instanceof HeaderKey hka && b instanceof HeaderValuePiece vcb ) {
+				return new Header(hka.key(), vcb.data());
+			}
+			if( a instanceof Header ha && b instanceof HeaderValuePiece vcb ) {
+				return new Header(ha.key(), ha.value()+vcb.data());
+			}
+			if( a instanceof HeaderValuePiece vca && b instanceof HeaderValuePiece vcb ) {
+				return new HeaderValuePiece(vca.data() + vcb.data());
+			}
+			if( a instanceof ContentPiece vca && b instanceof ContentPiece vcb ) {
+				return new ContentPiece(ArrayUtil.concat(vca.data(), 0, vcb.data()));
+			}
+			return null;
 		}
-		if( a instanceof HeaderValuePiece vca && b instanceof HeaderValuePiece vcb ) {
-			return new HeaderValuePiece(vca.data() + vcb.data());
+		
+		@Override
+		public DucerState<Chunk[], Chunk[]> apply(DucerChunk<Chunk[]> input) {
+			Chunk previousChunk = this.previousChunk;
+			List<Chunk> merged = new ArrayList<Chunk>();
+			for( Chunk c : input.payload ) {
+				if( c instanceof HeaderValuePiece hvc && hvc.data().length() == 0 ) continue;
+				if( c instanceof ContentPiece cc && cc.data().length == 0 ) continue;
+				
+				if( c instanceof HeaderKey hk ) c = new Header(hk.key(), "");
+				
+				if( previousChunk == null ) {
+					previousChunk = c;
+					continue;
+				}
+				
+				Chunk mergedChunk = merge(previousChunk, c);
+				if( mergedChunk == null ) {
+					merged.add(previousChunk);
+					previousChunk = c;
+				} else {			
+					previousChunk = mergedChunk;
+				}
+			}
+			if( input.isEnd() ) {
+				if( previousChunk != null ) {
+					merged.add(previousChunk);
+					previousChunk = null;
+				}
+			}
+			return new DucerState<Chunk[],Chunk[]>(
+				new LLChunkMerger(previousChunk),
+				EMPTY_CLOSED_INPUT,
+				new DucerChunk<Chunk[]>(merged.toArray(new Chunk[merged.size()]), input.isEnd())
+			);
 		}
-		if( a instanceof ContentPiece vca && b instanceof ContentPiece vcb ) {
-			return new ContentPiece(ArrayUtil.concat(vca.data(), 0, vcb.data()));
-		}
-		return null;
 	}
 	
 	protected static List<Chunk> normalize(List<Chunk> chunks) {
-		Chunk previousChunk = null;
-		List<Chunk> merged = new ArrayList<Chunk>();
-		for( Chunk c : chunks ) {
-			if( c instanceof HeaderValuePiece hvc && hvc.data().length() == 0 ) continue;
-			if( c instanceof ContentPiece cc && cc.data().length == 0 ) continue;
-			
-			if( c instanceof HeaderKey hk ) c = new Header(hk.key(), "");
-			
-			if( previousChunk == null ) {
-				previousChunk = c;
-				continue;
-			}
-			
-			Chunk mergedChunk = merge(previousChunk, c);
-			if( mergedChunk == null ) {
-				merged.add(previousChunk);
-				previousChunk = c;
-			} else {			
-				previousChunk = mergedChunk;
-			}
-		}
-		if( previousChunk != null ) merged.add(previousChunk);
-		return merged;
+		DucerState<Chunk[],Chunk[]> ds = LLChunkMerger.INIT.apply(new DucerChunk<Chunk[]>(chunks.toArray(new Chunk[chunks.size()]), true));
+		assert ds.isDone();
+		assert ds.input.queued.length == 0;
+		return Arrays.asList(ds.output.payload);
 	}
 	static List<Chunk> normalize(Chunk[] chunks) {
 		return normalize(Arrays.asList(chunks));
@@ -95,16 +127,12 @@ public class TEFParserTest extends TestCase
 		byte[] sourceBytes = source.getBytes(UTF8);
 		for( int s=0; s<20; ++s ) {
 			byte[][] chunks = randomlyChunk(sourceBytes, new Random(s));
-			for( byte[] chunk : chunks ) {
-				System.err.println("  \""+new String(chunk, UTF8)+"\"");
-			}
 			DucerState<byte[],Chunk[]> parseState = TEFParser.INIT.apply(new DucerChunk<byte[]>(new byte[0], false));
 			List<Chunk> output = new ArrayList<Chunk>();
 			for( int i=0; i<chunks.length; ++i ) {
 				parseState = parseState.process(new DucerChunk<byte[]>(chunks[i], i == chunks.length-1));
 				for(Chunk c : parseState.output.payload) {
 					output.add(c);
-					System.err.println("Got chunk "+c);
 				}
 			}
 			assertEquals(normalize(expected), normalize(output));
