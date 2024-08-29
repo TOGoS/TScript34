f9f0265a64cf11e4911ef3fb98ee3a507fdc4dfe
TOGoS <togos00@gmail.com>
2024-08-29 21:06:41 +0000

    TS34P19-v0.2.4

--- src/main/java/net/nuke24/tscript34/p0019/P0019.java
+++ src/main/java/net/nuke24/tscript34/p0019/P0019.java
@@ -38,7 +38,7 @@ import net.nuke24.tscript34.p0019.value.Symbol;
 
 public class P0019
 {
-	public static final String VERSION = "0.2.3-dev";
+	public static final String VERSION = "0.2.4";
 	public static final String PROGRAM_NAME = "TScript34-P0019-"+VERSION;
 
 	public static final Symbol MARK = new Symbol("http://ns.nuke24.net/TScript34/P0019/Constants/Mark");
