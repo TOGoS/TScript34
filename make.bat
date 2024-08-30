e8dd41a8cdedf295a25a250ddbc0a0836adaac7e
TOGoS <togos00@gmail.com>
2024-08-29 20:56:40 -0500

    Rename make.bat's target to be consistent with Makefile's

--- make.bat
+++ make.bat
@@ -11,7 +11,7 @@ rem 1.6 is what I want, but my JDKs can't do it
 set JAVAC_SOURCE_VERSION=1.7
 set JAVAC_TARGET_VERSION=1.7
 
-java -jar "%bootstrap_jar%" -o target/P0019.jar --include-sources --java-sources=src/main/java --main-class=net.nuke24.tscript34.p0019.cmd.P0019Command
+java -jar "%bootstrap_jar%" -o target/TS34P19-dev.jar --include-sources --java-sources=src/main/java --main-class=net.nuke24.tscript34.p0019.cmd.P0019Command
 
 goto eof
 
