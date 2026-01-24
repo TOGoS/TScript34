fc02a772a3a3f288d1a65790aa9e884d41f4fb6d
TOGoS <togos00@gmail.com>
2026-01-24 15:18:48 -0600

    More minor refactoring

--- src/interp01/chicken/run-tests.scm
+++ src/interp01/chicken/run-tests.scm
@@ -2,12 +2,12 @@
 (import (chicken irregex)
         (chicken io))
 
-(define (run-from-port port)
+(define (run-test-cases-from-port port)
   (let ((line (read-line port)))
     (if (not (eof-object? line))
         (begin
           (display (string-append "read: " line "\n"))
-          (run-from-port port)
+          (run-test-cases-from-port port)
           ))))
 
 (define (parse-args args)
@@ -16,14 +16,16 @@
 (define (get-input-files config)
   (cdr (assoc 'input-files config)))
 
+(define (call-with-input-pseudo-file filename port-action)
+  (if (string=? filename "-")
+    (port-action (current-input-port))
+    (call-with-input-file filename port-action)))
+
+(define (run-test-cases-from-file file) (call-with-input-pseudo-file file run-test-cases-from-port))
+
 (define (main args)
   (let* ((config (parse-args args))
          (files (get-input-files config)))
     (if (null? files)
         (display "WARNING: no test case files were indicated\n")
-        (for-each
-          (lambda (file)
-            (if (string=? file "-")
-                (run-from-port (current-input-port))
-                (call-with-input-file file run-from-port)))
-          files))))
+        (for-each run-test-cases-from-file files))))
