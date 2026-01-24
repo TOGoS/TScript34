b8cc8fe5a0bb264401e657cddc94fee995b837af
TOGoS <togos00@gmail.com>
2026-01-24 15:09:39 -0600

    Read test cases from indicated files

--- run-tests-with-interp01.bat
+++ run-tests-with-interp01.bat
@@ -11,4 +11,4 @@ echo CSI_EXE not defined and I don't know where to look for it.>&2
 exit/B 1
 
 :run
-%CSI_EXE% -ss src\interp01\chicken\run-tests.scm <test-cases.tsv
+%CSI_EXE% -ss src\interp01\chicken\run-tests.scm test-cases.tsv
--- src/interp01/chicken/run-tests.scm
+++ src/interp01/chicken/run-tests.scm
@@ -2,13 +2,20 @@
 (import (chicken irregex)
         (chicken io))
 
-(define (run)
-  (let ((line (read-line)))
+(define (run-from-port port)
+  (let ((line (read-line port)))
     (if (not (eof-object? line))
         (begin
           (display (string-append "read: " line "\n"))
-          (run)
+          (run-from-port port)
           ))))
 
 (define (main args)
-  (run))
+  (if (null? args)
+      (display "WARNING: no test case files were indicated\n")
+      (for-each
+        (lambda (file)
+          (if (string=? file "-")
+              (run-from-port (current-input-port))
+              (call-with-input-file file run-from-port)))
+        args)))
